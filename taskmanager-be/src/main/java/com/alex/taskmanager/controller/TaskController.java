c4ef62828eb33698d55b58e4fc93e63a8a355836
Alex <pich.alex01@gmail.com>
2020-11-05 14:03:31 -0800

    Enabled CORS

--- taskmanager-be/src/main/java/com/alex/taskmanager/controller/TaskController.java
+++ taskmanager-be/src/main/java/com/alex/taskmanager/controller/TaskController.java
@@ -7,6 +7,7 @@ import javax.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.alex.taskmanager.model.Task;
 import com.alex.taskmanager.service.TaskService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TaskController {
 
 	@Autowired
